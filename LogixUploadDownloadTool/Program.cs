b3e73a2eea941fe25b7f4c49c18047850f584a9f
Ron Beyer <rbeyer@controlaware.com>
2023-10-15 13:14:14 -0400

    Fixes #1, issue was not setting the project path prior to connecting.

--- LogixUploadDownloadTool/Program.cs
+++ LogixUploadDownloadTool/Program.cs
@@ -40,9 +40,6 @@ namespace LogixUploadDownloadTool
         /// <returns>Runtime Code</returns>
         static RuntimeErrorCodes UploadIntoExistingFile(UploadOptions options)
         {
-            ConsoleLog.LogError("Uploading into an existing file is not currently supported.");
-            return RuntimeErrorCodes.UnknownError;
-
             LogixServicesLifetimeManager? svcManager = null;
             RSLogix5000ServicesLib.Controller? controller = null;
 
@@ -83,6 +80,7 @@ namespace LogixUploadDownloadTool
             try
             {
                 ConsoleLog.Verbose("Connecting to controller...");
+                controller.CurrentCommPath = options.Path;
                 controller.GoOnline();
 
                 ConsoleLog.Verbose("Checking correlation status...");
